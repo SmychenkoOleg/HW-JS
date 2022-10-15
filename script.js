b7132502a6f03ce2a5270d55721e126d84e79a1f
Oleg Smychenko <smychenkooleg@gmail.com>
2022-10-15 13:52:14 +0300

    Add a litle humor

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-
+const windowInnerWidth = window.screen.availWidth;
 let w = 100;
 
 function blockWidth1(){
+  if (w <= windowInnerWidth ) {
   w++;
   document.querySelector('.out1').style.width = w + 'px';
   console.log(w);
+  }
+  else{
+    alert ('Та достатньо фігнею займатися! Йди роби домашку у Ксендзова! ))))');
+    w = 100;
+  }
 }
 
 function blockWidth2(){
+  if (w <= windowInnerWidth ) {
   w++;
   document.querySelector('.out2').style.width = w + 'px';
   console.log(w);
+  }
+  else{
+    alert ('Та достатньо фігнею займатися! Йди роби домашку у Ксендзова! ))))');
+    w = 100;
+  }
 }
 
-document.querySelector('.out1').addEventListener('mousemove', blockWidth1);
 
-document.querySelector('.out2').addEventListener('mousemove', blockWidth2);
+
+  document.querySelector('.out1').addEventListener('mousemove', blockWidth1);
+  document.querySelector('.out2').addEventListener('mousemove', blockWidth2);
 
 
 
